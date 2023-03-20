46f253c5ee2ea3ca6ac6926ce1fd2c0c28408158
KimByungjun3 <kkbb007@naver.com>
2023-03-20 23:15:18 +0900

    removed hello.java and added menumanager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,13 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Plan Exercise Routine");
+		System.out.println("2. Plan Tomorrow Exercise");
+		System.out.println("3. Write Exercise");
+		System.out.println("4. Write Weight");
+		System.out.println("5. Exit");
+
+	}
+
+}
