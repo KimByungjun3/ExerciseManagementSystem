d24e865de5c79d035461547424ed696c4b106157
KimByungjun3 <kkbb007@naver.com>
2023-03-27 23:08:18 +0900

    add menu selection

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,13 +1,39 @@
+import java.util.Scanner;
 
 public class MenuManager {
 
 	public static void main(String[] args) {
+		int num = 0;
+		Scanner  input = new Scanner(System.in);
+		
+		while (num != 6) {
 		System.out.println("1. Plan Exercise Routine");
 		System.out.println("2. Plan Tomorrow Exercise");
-		System.out.println("3. Write Exercise");
+		System.out.println("3. Write Today Exercise");
 		System.out.println("4. Write Weight");
-		System.out.println("5. Exit");
-
+		System.out.println("5. Show a Menu");
+		System.out.println("6. Exit");
+		System.out.println("Select one number between 1-6:");
+		num = input.nextInt();
+		if (num == 1) {
+			System.out.print("Plan exercise Routine: ");
+			String exerciseName = input.next();
+		}
+		else if (num == 2) {
+			System.out.print("Plan Tomorrow Eercise: ");
+			String tomorrowName = input.next();
+		}
+		else if (num == 3) {
+			System.out.print("Write Today Exercise: ");
+			String todayName = input.next();
+		}
+		else if (num == 4) {
+			System.out.print("Write Weight: ");
+			String weightName = input.next();
+		}
+		else {
+				continue;
+		}
+		}
+		} 
 	}
-
-}
